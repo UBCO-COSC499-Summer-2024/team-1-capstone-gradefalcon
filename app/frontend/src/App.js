699ac92e0decf9cab119061685859f21577c9548
BennettWitt <wittbennett@gmail.com>
2024-06-12 14:32:34 -0700

    Makes navigation bar appear on all pages by default and adjusts CSS accordingly. *Will need to delete old navigation bar from specific pages

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,13 +1,26 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import logo from "./assets/logo.png";
 import "./css/App.css";
 import "./css/style.css";
-
+import NavBar from "./NavBar";
 // Import pages
 import Dashboard from './pages/Instructor/Dashboard';
 import NotFound from './pages/NotFound';
 import InstructorSignup from './pages/Instructor/Signup';
+
+// Layout component to conditionally render NavBar
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const shouldDisplayNavBar = location.pathname !== '/';
+
+  return (
+    <>
+      {shouldDisplayNavBar && <NavBar />}
+      {children}
+    </>
+  );
+};
 
 function App() {
   const [message, setMessage] = useState("");
@@ -18,10 +31,12 @@ function App() {
       .then(res => setMessage(res.message))
       .catch(console.error);
   }, []);
+  
 
   return (
     <Router>
       <div className="App">
+        
         {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>{message || "Loading..."}</p>
@@ -29,15 +44,16 @@ function App() {
             Welcome to GradeFalcon, an optical marking system for grading bubble sheets.
           </p>
         </header> */}
+        <Layout>
         <Routes>
           <Route path="/" element={<InstructorSignup />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/signup" element={<InstructorSignup />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        </Layout>
       </div>
     </Router>
   );
 }
-
 export default App;
