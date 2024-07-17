baf8dcef368626b831fa026f40896755c1a0bb98
Ahmad Mirza <112994730+ahmadsm1@users.noreply.github.com>
2024-07-16 19:02:55 -0700

    Add loading screen while omr processes

--- app/frontend/src/pages/Instructor/OMRProcessing.js
+++ app/frontend/src/pages/Instructor/OMRProcessing.js
@@ -1,28 +1,38 @@
-import React, { useState, useEffect, useCallback, useReducer } from "react";
-import { useLocation, Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import Toast from "../../components/Toast";
 import "../../css/App.css";
 
 const OMRProcessing = (props) => {
   const location = useLocation();
+  const [toast, setToast] = useState(null);
   const { examTitle, classID } = location.state || {};
   const navigate = useNavigate();
 
   const runOMR = async () => {
     console.log("Running OMR");
-    const response = await fetch("/api/exam/callOMR", {
-      method: "POST",
-      credentials: "include",
-    });
-    const data = await response.json();
-    console.log("response", response);
-    console.log("data", data);
-    console.log("finished");
-    navigate("/ConfirmExamKey", {
-      state: {
-        examTitle: examTitle,
-        classID: classID,
-      },
-    });
+    try {
+      const response = await fetch("/api/exam/callOMR", {
+        method: "POST",
+        credentials: "include",
+      });
+      const data = await response.json();
+      console.log("response", response);
+      console.log("data", data);
+      console.log("finished");
+      setToast({ message: "Sheet successfully added!", type: "success" });
+      setTimeout(() => {
+        navigate("/ConfirmExamKey", {
+          state: {
+            examTitle: examTitle,
+            classID: classID,
+          },
+        });
+      }, 2000);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setToast({ message: "Failed to add sheet!", type: "error" });
+    }
   };
 
   useEffect(() => {
@@ -38,9 +48,31 @@ const OMRProcessing = (props) => {
     <>
       <div className="App">
         <div className="main-content">
+          <style>
+            {`
+                .App .main-content {
+                  display: flex;
+                  flex-direction: column;
+                  align-items: center;
+                  justify-content: center;
+                  height: 100vh; /* Adjust as needed */
+                }
+              `}
+          </style>
+          {toast && (
+            <>
+              <Toast
+                message={toast.message}
+                type={toast.type}
+                onClose={() => setToast(null)}
+              />
+              <style>{`.circle-loader { display: none; }`}</style>
+            </>
+          )}
           <header>
             <h2>Scanning file</h2>
           </header>
+          <div class="loader"></div>
         </div>
       </div>
     </>
