5768a1bee94d1ca26fe2cfcd73362a1bc421a62e
Ahmad Mirza <112994730+ahmadsm1@users.noreply.github.com>
2024-08-02 09:28:47 -0700

    fix

--- app/frontend/src/pages/Instructor/ConfirmExamKey.js
+++ app/frontend/src/pages/Instructor/ConfirmExamKey.js
@@ -123,24 +123,13 @@ const ConfirmExamKey = () => {
           question: i,
           option: optionSpan.innerText,
         });
-        // Check if this option is selected and apply styles
-        if (
-          selectedOptions.some(
-            (question) =>
-              question.question === i && question.option === optionSpan.innerText
-          )
-        ) {
-          optionSpan.classList.add("selected");
-          optionSpan.style.backgroundColor = "hsl(var(--primary))";
-          optionSpan.style.color = "white";
-        }
 
         optionsDiv.appendChild(optionSpan);
       }
 
       bubbleGrid.appendChild(questionDiv);
     }
-  }, [numQuestions, numOptions, selectedOptions]);
+  }, [numQuestions, numOptions]);
 
   useEffect(() => {
     setTotalMarks(numQuestions);
