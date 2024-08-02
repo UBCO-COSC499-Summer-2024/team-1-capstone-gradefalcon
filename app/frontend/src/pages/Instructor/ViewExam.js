9a1c757a0479ac6fe07397619f5f9c42203a19ce
Ahmad Mirza <112994730+ahmadsm1@users.noreply.github.com>
2024-08-02 06:13:39 -0700

    Put images on view exam side by side (temp fix)

--- app/frontend/src/pages/Instructor/ViewExam.js
+++ app/frontend/src/pages/Instructor/ViewExam.js
@@ -1,31 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import "../../css/App.css";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const ViewExam = () => {
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const { isAuthenticated, getAccessTokenSilently } = useAuth0();
   const location = useLocation();
   const navigate = useNavigate();
   let { student_id, exam_id, front_page, back_page } = location.state || {};
-  const [examFileId, setExamFileId] = useState("");
   const [frontSrc, setFrontSrc] = useState("");
   const [backSrc, setBackSrc] = useState("");
 
-  // send the student_id and exam_id to a backend function
-  // backend function reads the csv file, matches the student_id and returns exam file id
-  // frontend fetches the exam file id and displays the exam file
-
   useEffect(() => {
     const fetchExam = async () => {
-      const token = await getAccessTokenSilently(); // Get the token
+      const token = await getAccessTokenSilently();
       if (!student_id) {
         console.log("Student ID is missing");
         return;
       }
       try {
-        if(front_page === undefined || back_page === undefined) {
-          // This means we are viewing the exam from the examboard, not after the results have been read
+        if (front_page === undefined || back_page === undefined) {
           front_page = `../../../../../uploads/Students/exam_id_${exam_id}/student_id_${student_id}/front_page.png`;
           back_page = `../../../../../uploads/Students/exam_id_${exam_id}/student_id_${student_id}/back_page.png`;
         }
@@ -37,8 +31,6 @@ const ViewExam = () => {
           },
           body: JSON.stringify({ side: "back", file_name: back_page }),
         });
-        // const data2 = await responseImage.json();
-        // console.log(data2);
         let blob = await back_page_response.blob();
         let url = URL.createObjectURL(blob);
         setBackSrc(url);
@@ -51,50 +43,47 @@ const ViewExam = () => {
           },
           body: JSON.stringify({ side: "front", file_name: front_page }),
         });
-        // const data2 = await responseImage.json();
-        // console.log(data2);
         blob = await front_page_response.blob();
         url = URL.createObjectURL(blob);
         setFrontSrc(url);
-
-        // setExamFileId(front_page);
       } catch (error) {
         console.error("Failed to fetch exam:", error);
       }
     };
 
     fetchExam();
-  }, [student_id, examFileId, getAccessTokenSilently, isAuthenticated]);
+  }, [student_id, getAccessTokenSilently, isAuthenticated]);
 
   return (
     <div className="App">
       <div className="main-content">
         <h1>View Exam</h1>
-
-        {backSrc ? (
-          <img
-            src={backSrc}
-            alt="Student Answers"
-            style={{
-              maxWidth: "30%",
-              height: "auto",
-            }}
-          />
-        ) : (
-          <p>Loading image...</p>
-        )}
-        {frontSrc ? (
-          <img
-            src={frontSrc}
-            alt="Student ID"
-            style={{
-              maxWidth: "30%",
-              height: "auto",
-            }}
-          />
-        ) : (
-          <p>Loading image...</p>
-        )}
+        <div className="image-container">
+          {frontSrc ? (
+            <img
+              src={frontSrc}
+              alt="Student ID"
+              style={{
+                maxWidth: "30%",
+                height: "auto",
+              }}
+            />
+          ) : (
+            <p>Loading image...</p>
+          )}
+          {backSrc ? (
+            <img
+              src={backSrc}
+              alt="Student Answers"
+              style={{
+                maxWidth: "30%",
+                height: "auto",
+              }}
+            />
+          ) : (
+            <p>Loading image...</p>
+          )}
+        </div>
         <div>
           <button className="save-changes-btn" onClick={() => navigate(-1)}>
             Go Back
