e8ff3a17c113ed195496693f008f2b79f41fa2c8
Ahmad Mirza <112994730+ahmadsm1@users.noreply.github.com>
2024-08-04 16:51:06 -0700

    Display exams side by side

--- app/frontend/src/pages/Instructor/ViewExam.js
+++ app/frontend/src/pages/Instructor/ViewExam.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "../../css/App.css";
+import { Card, CardContent } from "../../components/ui/card";
+import { Label } from "../../components/ui/label";
+import { RadioGroup, RadioGroupItem } from "../../components/ui/radio-group";
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 const ViewExam = () => {
@@ -10,6 +14,8 @@ const ViewExam = () => {
   let { student_id, exam_id, front_page, back_page } = location.state || {};
   const [frontSrc, setFrontSrc] = useState("");
   const [backSrc, setBackSrc] = useState("");
+  const [imageLinks, setImageLinks] = useState([]);
+  const [originalBack, setOriginalBack] = useState("");
 
   useEffect(() => {
     const fetchExam = async () => {
@@ -27,7 +33,7 @@ const ViewExam = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
+            Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({ side: "back", file_name: back_page }),
         });
@@ -39,7 +45,7 @@ const ViewExam = () => {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`,
+            Authorization: `Bearer ${token}`,
           },
           body: JSON.stringify({ side: "front", file_name: front_page }),
         });
@@ -58,36 +64,42 @@ const ViewExam = () => {
     <div className="App">
       <div className="main-content">
         <h1>View Exam</h1>
-        <div className="image-container">
-          {frontSrc ? (
-            <img
-              src={frontSrc}
-              alt="Student ID"
-              style={{
-                maxWidth: "30%",
-                height: "auto",
-              }}
-            />
-          ) : (
-            <p>Loading image...</p>
-          )}
-          {backSrc ? (
-            <img
-              src={backSrc}
-              alt="Student Answers"
-              style={{
-                maxWidth: "30%",
-                height: "auto",
-              }}
-            />
-          ) : (
-            <p>Loading image...</p>
-          )}
-        </div>
-        <div>
-          <button className="save-changes-btn" onClick={() => navigate(-1)}>
-            Go Back
-          </button>
+        <div className="w-full">
+          <Card className="bg-white border rounded">
+            <CardContent>
+              <div className="flex space-x-4">
+                {frontSrc ? (
+                  <img
+                    src={frontSrc}
+                    alt="Student ID"
+                    style={{
+                      maxWidth: "40%",
+                      height: "auto",
+                    }}
+                  />
+                ) : (
+                  <p>Loading image...</p>
+                )}
+                {backSrc ? (
+                  <img
+                    src={backSrc}
+                    alt="Student Answers"
+                    style={{
+                      maxWidth: "40%",
+                      height: "auto",
+                    }}
+                  />
+                ) : (
+                  <p>Loading image...</p>
+                )}
+              </div>
+            </CardContent>
+          </Card>
+          <div>
+            <button className="save-changes-btn" onClick={() => navigate(-1)}>
+              Go Back
+            </button>
+          </div>
         </div>
       </div>
     </div>
